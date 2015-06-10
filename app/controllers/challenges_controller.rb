2753a10e91186b4dc38b99ee4b014e1d00cdb30f
guzmanweb <carlos@guzmanweb.com>
2015-06-10 17:51:47 +0000

    Add new challenge view, check validations styles, add partial notice to layout

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -3,4 +3,25 @@ class ChallengesController < ApplicationController
   def index
   end
 
+  def new
+    @challenge = Challenge.new
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+    
+    if @challenge.save(challenge_params)
+      flash.now[:notice] = "New challenge has been created"
+    else
+      flash.now[:alert] = "An error has ocurred"
+      render :new
+    end
+  end
+
+  private
+
+    def challenge_params
+      params.require(:challenge).permit(:from, :to)
+    end
+
 end
